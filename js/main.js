a54309fdf2b52e31ef01cb615c22dce883d39c59
RandhyBryan <49753582+RandhyBryanCroenField@users.noreply.github.com>
2020-04-09 16:10:48 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const input = document.querySelectorAll(".input");
+
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+input.forEach(input => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
+});
